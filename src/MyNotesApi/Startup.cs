6c34bc9ba1a41a651543848f01f2307d24c60e17
Bruno Lamarao <Bruno.Lamarao@coolblue.nl>
2020-12-13 18:11:50 +0100

    adds swagger

--- src/MyNotesApi/Startup.cs
+++ src/MyNotesApi/Startup.cs
@@ -27,6 +27,9 @@ namespace MyNotesApi
         {
             services.AddControllers();
 
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+
             services.AddSimpleInjector(_container, options =>
             {
                 options
@@ -54,6 +57,16 @@ namespace MyNotesApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             RegisterComponents(app);
 
